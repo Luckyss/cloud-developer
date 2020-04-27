012516176235b96a5f23386203c387f1e0e52985
Sergey Antonov <antssergey@gmail.com>
2020-04-27 16:48:21 +0300

    refactor: split logic to two microservices. This one is for 'feed'.

---
+++ course-02/exercises/udacity-c2-feed-microservice/src/controllers/v0/feed/routes/feed.router.ts
@@ -0,0 +1,133 @@
+import {Router, Request, Response, NextFunction} from 'express';
+import {FeedItem} from '../models/FeedItem';
+import * as jwt from 'jsonwebtoken';
+import * as AWS from '../../../../aws';
+import {config} from '../../../../config/config';
+
+const router: Router = Router();
+
+// Get all feed items
+router.get('/', async (req: Request, res: Response) => {
+    const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
+    items.rows.map((item) => {
+        if (item.url) {
+            item.url = AWS.getGetSignedUrl(item.url);
+        }
+    });
+    res.send(items);
+});
+
+// GET a specific resource by Primary Key
+router.get('/:id',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        const {id} = req.params;
+        const item = await FeedItem.findByPk(id);
+        if (!item) {
+            return res.status(404)
+                .send({error: 'This item doesn\'t exist'});
+        } else {
+            res.send(item);
+        }
+    });
+
+// update a specific resource
+router.patch('/:id',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        const {id} = req.params;
+        const feedItem: FeedItem = req.body;
+
+        if (feedItem == null) {
+            return res.status(400)
+                .send({error: 'body can\'t be empty'});
+        }
+
+        if (feedItem.caption || feedItem.url) {
+            if (feedItem.id && feedItem.id !== id) {
+                return res.status(400)
+                    .send({error: 'id in body doesn\'t equal to id in parameters'});
+            }
+            const item = await FeedItem.findByPk(id)
+                .then(feedItemInstance => {
+                    if (feedItemInstance) {
+                        feedItemInstance.update(feedItem, {
+                            where: {id: id}
+                        }).then(item1 => {
+                            res.status(200)
+                                .send(item1);
+                        }).catch(error => {
+                            res.status(500)
+                                .send({error: error});
+                        });
+                    } else {
+                        return res.status(404)
+                            .send({error: 'item with this id doesn\'t exist'});
+                    }
+                }).catch(error => {
+                    res.status(500)
+                        .send({error: error});
+                });
+        }
+    });
+
+// Get a signed url to put a new item in the bucket
+router.get('/signed-url/:fileName',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        const {fileName} = req.params;
+        const url = AWS.getPutSignedUrl(fileName);
+        res.status(201).send({url: url});
+    });
+
+// Post meta data and the filename after a file is uploaded
+// NOTE the file name is they key name in the s3 bucket.
+// body : {caption: string, fileName: string};
+router.post('/',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        const caption = req.body.caption;
+        const fileName = req.body.url;
+
+        // check Caption is valid
+        if (!caption) {
+            return res.status(400).send({message: 'Caption is required or malformed'});
+        }
+
+        // check Filename is valid
+        if (!fileName) {
+            return res.status(400).send({message: 'File url is required'});
+        }
+
+        const item = await new FeedItem({
+            caption: caption,
+            url: fileName
+        });
+
+        const saved_item = await item.save();
+
+        saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+        res.status(201).send(saved_item);
+    });
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+    if (!req.headers || !req.headers.authorization) {
+        return res.status(401).send({message: 'No authorization headers.'});
+    }
+
+    const token_bearer = req.headers.authorization.split(' ');
+    if (token_bearer.length !== 2) {
+        return res.status(401).send({message: 'Malformed token.'});
+    }
+
+    const token = token_bearer[1];
+
+    return jwt.verify(token, config.jwt.secret, (err, decoded) => {
+        if (err) {
+            return res.status(500).send({auth: false, message: 'Failed to authenticate.'});
+        }
+        return next();
+    });
+}
+
+export const FeedRouter: Router = router;
